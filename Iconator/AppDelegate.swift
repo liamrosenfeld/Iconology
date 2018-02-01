783d72baaf60fdd54b1697c31e8cdd4f632fb21b
Liam Rosenfeld <liamrosenfeld@gmail.com>
2018-02-01 17:59:41 -0500

    Initial Commit

---
+++ Iconator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Iconator
+//
+//  Created by Liam Rosenfeld on 2/1/18.
+//  Copyright © 2018 Liam Rosenfeld. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+#  Iconator
+I got tired of manually exporting every icon size, so I made a app to do it.
+
