23eab84ec348c84c2395ba42fc974bdf8c97a057
Liam Rosenfeld <liamrosenfeld@gmail.com>
2018-02-03 14:27:08 -0500

    Able to access settings in options view
    TODO:
    - Toggle between iOS and Mac icons
    - Add functionality
    - Add save location option

--- Iconator/OptionsViewController.swift
+++ Iconator/OptionsViewController.swift
@@ -10,11 +10,47 @@ import Cocoa
 
 class OptionsViewController: NSViewController {
     
+    // Setup
+    @IBOutlet weak var iosVersion: NSPopUpButton!
+    @IBOutlet weak var iPhoneToggle: NSButton!
+    @IBOutlet weak var iPadToggle: NSButton!
+    
     var imageURL: NSURL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(imageURL!)
+    }
+    
+    // Actions
+    @IBAction func convert(_ sender: Any) {
+        let iosVersionString = iosVersion.titleOfSelectedItem!
+        let iPhoneEnabled = iPhoneToggle.state.rawValue
+        let iPadEnabled = iPadToggle.state.rawValue
+        
+        if iosVersionString == "11" {
+            if iPhoneEnabled == 1 {
+                
+            }
+            if iPadEnabled == 1 {
+                
+            }
+        } else if iosVersionString == "10" {
+            if iPhoneEnabled == 1 {
+                
+            }
+            if iPadEnabled == 1 {
+                
+            }
+        } else if iosVersionString == "11" {
+            if iPhoneEnabled == 1 {
+                
+            }
+            if iPadEnabled == 1 {
+                
+            }
+        }
+        
     }
     
 }
