af1069f14950c779a4bd1a3cfa0d020b32cdbd46
Liam Rosenfeld <liamrosenfeld@gmail.com>
2018-02-15 21:16:51 -0500

    New window pops up on dock click (as long as no other window is open)

--- Iconator/AppDelegate.swift
+++ Iconator/AppDelegate.swift
@@ -11,8 +11,6 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
@@ -20,7 +18,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag {
+            for window: AnyObject in sender.windows {
+                window.makeKeyAndOrderFront(self)
+            }
+        }
+        return true
+    }
+    
 }
 
