b8653c009d15fc30454b4d92df34c4b2a1a5aac1
Liam Rosenfeld <liamrosenfeld@gmail.com>
2018-05-19 12:04:04 -0400

    Added Edit Preset Views

---
+++ Iconizer/ViewControllers/EditPresetViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EditPresetViewController.swift
+//  Iconizer
+//
+//  Created by Liam Rosenfeld on 5/19/18.
+//  Copyright © 2018 Liam Rosenfeld. All rights reserved.
+//
+
+import Cocoa
+
+class EditPresetViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Iconizer/ViewControllers/SelectPresetViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SelectPresetViewController.swift
+//  Iconizer
+//
+//  Created by Liam Rosenfeld on 5/19/18.
+//  Copyright © 2018 Liam Rosenfeld. All rights reserved.
+//
+
+import Cocoa
+
+class SelectPresetViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
