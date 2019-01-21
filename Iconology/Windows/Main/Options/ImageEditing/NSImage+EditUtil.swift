7fceaf30ee604a406a27f3ba8e3c4f092ca9600b
Liam Rosenfeld <liamrosenfeld@users.noreply.github.com>
2019-01-21 17:14:03 -0500

    Rounding

---
+++ Iconology/Windows/Main/Options/ImageEditing/NSImage+EditUtil.swift
@@ -0,0 +1,26 @@
+//
+//  NSImage+EditUtil.swift
+//  Iconology
+//
+//  Created by Liam on 1/18/19.
+//  Copyright © 2019 Liam Rosenfeld. All rights reserved.
+//
+
+import AppKit
+
+extension NSImage {
+    func makeRep(at size: NSSize) -> NSBitmapImageRep {
+        let rep = NSBitmapImageRep(bitmapDataPlanes: nil,
+                                   pixelsWide: Int(size.width),
+                                   pixelsHigh: Int(size.height),
+                                   bitsPerSample: 8,
+                                   samplesPerPixel: 4,
+                                   hasAlpha: true,
+                                   isPlanar: false,
+                                   colorSpaceName: .calibratedRGB,
+                                   bytesPerRow: 0,
+                                   bitsPerPixel: 0)
+        return rep!
+    }
+}
+
